42bcc3778620972c5fb57f5631976cd4ab419fb6
Vladimir Vozar <vladimirvozar@gmail.com>
2020-04-27 17:27:09 +0200

    Initial commit

---
+++ ApiVersioning/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiVersioning.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public WeatherForecastResult Get()
+        {
+            var rng = new Random();
+            
+            var result = new WeatherForecastResult
+            {
+                ApiVersion = "",
+                WeatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index).ToString(),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+            .ToArray()
+            };
+
+            return result;
+        }
+    }
+}
---
+++ ApiVersioning/WeatherForecastResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiVersioning
+{
+    public class WeatherForecastResult
+    {
+        public string ApiVersion { get; set; }
+        public WeatherForecast[] WeatherForecasts { get; set; }
+    }
+}
